89bb1356f2b76757349451336428e08d77772523
nitinsayshe <nitinsayshe@gmail.com>
2022-07-07 15:15:03 +0530

    naS

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,21 +1,27 @@
 const reviewModel = require("../models/reviewModel")
 const bookModel = require("../models/bookModel")
 const ObjectId = require('mongoose').Types.ObjectId;
+const moment=require("moment")
+
+
 
 
 const addReview = async (req, res) => {
     let { bookId } = req.params
     let { reviewedBy, reviewedAt, rating, review } = req.body
+    let regEx=/^[A-Za-z -.]+$/
 
     if (!ObjectId.isValid(bookId)) return res.status(400).send({ status: false, message: "Book Id is Invalid !!!!" })
 
     let findBook = await bookModel.findOne({ bookId: bookId, isDeleted: false })
     if (!findBook) return res.status(404).send({ status: false, message: "No such Book is Present as Per BookID" })
+    if(!reviewedAt) return res.status(400).send({status:false,message:"reviewedAt date is Missing"})
 
-   
-   // if(reviewedBy && reviewedBy.length==0) return res.status(400).send({ status: false, message: " Plz enter Valid reviewedBY" })
-    // if (reviewedBy && typeof (reviewedBy) !== "string") return res.status(400).send({ status: false, message: " Plz enter Valid reviewedBY" })
-    //if (reviewedBy && (!reviewedBy.trim())) return res.status(400).send({ status: false, message: " Plz enter Valid reviewedBY" })
+    if(reviewedBy &&  (reviewedBy==="") || !(regEx.test(reviewedBy))) return res.status(400).send({status:false,message:"invalid Value in reviewed BY!!!"})
+
+    if(!moment(reviewedAt,"YYYY-MM-DD",true).isValid) return res.status(400).send({status:false,message:false})
+    var d = moment("2016-10-19").format(dateFormat);
+
 
     req.body.bookId = bookId.toString()
     let reviewDate = await reviewModel.create(req.body)
